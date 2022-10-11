509540f76344053f0b6c7aea9d8460a61cbadda9
eli-okt <108367319+eli-okt@users.noreply.github.com>
2022-10-11 10:12:46 +0300

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+var countDownDate = new Date("Oct 20, 2022 19:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
